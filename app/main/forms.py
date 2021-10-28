bdb343ee50d883b5f879844e4dfe95aaaa513da5
sling254 <mwanikidenis7@gmail.com>
2021-10-28 12:17:20 +0300

    intial commit

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required, Email
+
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
